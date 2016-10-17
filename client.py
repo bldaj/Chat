71c42d2a24111ecb804f31d2801a5705d7b85f94
Konstantin Kaloshin <wetarmor@gmail.com>
2016-10-17 17:03:43 +0400

    working client on threading

--- client.py
+++ client.py
@@ -1,13 +1,29 @@
 import socket
+import _thread
+
+data = ''
+
+
+def recv_data(sock, data):
+    while True:
+        data = sock.recv(1024).decode()
+        print(data)
+
+
+def send_data(sock):
+    while True:
+        sock.send(input().encode())
+
 
 sock = socket.socket()
 
 sock.connect(('127.0.0.1', 8080))
-sock.send('hello'.encode())
+
+
+_thread.start_new_thread(recv_data, (sock, data))
+_thread.start_new_thread(send_data, (sock, ))
 
 while True:
-    data = sock.recv(1024).decode()
-    print(data)
-    sock.send(input().encode())
+    pass
 
-sock.close()
+
